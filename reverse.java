26fef00ce05846eaca5b310d13f6acad29548d73
kalaiyarasikalai <pkalaiyarasi.ksrct@gmail.com>
2016-08-09 18:41:54 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -6,6 +6,7 @@ public static void main(String[] args)
 Scanner s=new Scanner(System.in);
 int n=s.nextInt();
 int i=n,a,count=0;
+if(n>0){
 while(n>0)
 {
 a=n%10;
@@ -16,6 +17,12 @@ int a[]=new int[count];
 for(int j=0;j<a.length;j++)
 {
 sum+=pow(10,j)*a[j];
+}
+}
+else
+{
+  System.out.println(0);
+}
 }
 }
 }
