79fb2dc746fcde2779af7908b7cda165ae7c09a5
kalaiyarasikalai <pkalaiyarasi.ksrct@gmail.com>
2016-08-09 18:38:08 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class reverse
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int i=n,a,count=0;
+while(n>0)
+{
+a=n%10;
+n=n/10;
+count++;
+}
+int a[]=new int[count];
+for(int j=0;j<a.length;j++)
+{
+sum+=pow(10,j)*a[j];
+}
+}
+}
