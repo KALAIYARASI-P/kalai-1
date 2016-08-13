b679f1735df213fdc35eabcc8ba954653be8ff22
kalaiyarasikalai <pkalaiyarasi.ksrct@gmail.com>
2016-08-13 09:08:58 +0530

    Update small.java

--- small.java
+++ small.java
@@ -5,10 +5,15 @@ public static void main(String[] args)
 {
 Scanner s=new Scanner(System.in);
 int n=s.nextInt();
+if(n>0)
+{
 int[] a=new int[n];
 for(int i=0;i<n;i++)
 a[i]=s.nextInt();
 Arrays.sort(a);
 System.out.println(a[1]);
+}
+else
+System.out.println(0);
 }
 }
