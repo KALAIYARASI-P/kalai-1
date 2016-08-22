dd00c2cb0ae1d6efad98bbb07e1ee649545dc889
kalaiyarasikalai <pkalaiyarasi.ksrct@gmail.com>
2016-08-22 19:01:04 +0530

    Create st.java

---
+++ st.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class st
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String a="5";
+String b="2";
+int c=Integer.parseInt(a);
+int d=Integer.parseInt(b);
+System.out.println(c*d);
+}
+}
