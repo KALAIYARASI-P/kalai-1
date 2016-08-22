411db2af4bb63583b510a5d192f982cb9c2da190
kalaiyarasikalai <pkalaiyarasi.ksrct@gmail.com>
2016-08-22 09:26:06 +0530

    Create reversestring.java

---
+++ reversestring.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class reversestring
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+StringBuffer ss=s.next();
+ss.reverse();
+System.out.println(ss);
+}
+}
