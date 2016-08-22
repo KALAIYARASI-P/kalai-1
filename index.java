beadb1f8ed1c9e719893497d11140a0a680839f4
kalaiyarasikalai <pkalaiyarasi.ksrct@gmail.com>
2016-08-22 19:30:32 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class index
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[i]=new int[n];
+for(int i=0;i<n;i++)
+a[i]=s.nextInt();
+for(int i=0;i<n;i++)
+{
+ if(a[i]==i)
+ System.out.println(a[i]);
+ }
+ 
+}
+}
+ 
