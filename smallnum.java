eb267c46939f020bb6e667ad4b75192450f85901
kalaiyarasikalai <pkalaiyarasi.ksrct@gmail.com>
2016-08-24 15:19:22 +0530

    Update and rename small.java to smallnum.java

---
+++ smallnum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class smallnum
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String n=s.next();
+int k=s.nextInt();
+int a[]=new int[n.length()];
+int n=Integer.parseInt(n);
+int i=0;
+while(n>0)
+{
+int c=n%10;
+a[i++]=c;
+n=n/10;
+}
+Arrays.sort(a);
+for(int j=0;j<k;i++)
+System.out.print(a[j]);
+}
+}
