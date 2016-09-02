db4b83bfd9323075a0b9762413b9cd53d9bbf784
kalaiyarasikalai <pkalaiyarasi.ksrct@gmail.com>
2016-09-02 12:09:53 +0530

    Create pro15.java

---
+++ pro15.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class pro15
+{
+public static void main(String args[])
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[]=new int[n];
+int b[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=s.nextInt();
+}
+for(int i=0;i<n;i++)
+{b[i]=0;
+while(a[i]>0)
+{
+int c=a[i]%2;
+b[i]+=c;
+a[i]=a[i]/2;
+}
+}
+int t=0;
+for(int i=0;i<n;i++)
+{
+for(int j=i+1;j<n;j++)
+{
+ if(b[i]<b[j])
+ {
+  t=b[i];
+  b[i]=b[j];
+  b[j]=t;
+  t=a[i];
+  a[i]=a[j];
+  a[j]=t;
+  }
+  if(b[i]=b[j])
+  {
+   if(a[i]<a[j])
+   {
+    t=b[i];
+  b[i]=b[j];
+  b[j]=t;
+  t=a[i];
+  a[i]=a[j];
+  a[j]=t;}
+  }
+  }
+  }
+  for(int i:a)
+  System.out.println(i);
+  }
+  }
